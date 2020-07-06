3322239c2524c601e6fca7009ca0c30b5863bfdb
Tremeschin <tremeschin@protonmail.com>
2020-07-06 02:35:53 -0400

    Comment (a lot) / code review / optimizations / warnings frame.py

--- src/frame.py
+++ src/frame.py
@@ -39,43 +39,58 @@ class Frame():
 
     # Create new numpy array as a "frame", attribute width, height and resolution
     def new(self, width, height, transparent=False):
+
+        # Transparent is RGBA, not transparent is RGB
         if transparent:
             channels = 4
         else:
             channels = 3
-            
+        
+        # Blank zeros frame based on transparency option
         self.frame = np.zeros([height, width, channels], dtype=np.uint8)
+
+        # This is our "loaded" image, as we're creating a new, it's the original one
         self.original_image = Image.fromarray(self.frame)
+
+        # Update width, height info
         self.width = width
         self.height = height
-        self.resolution = (width, height)
-        self.name = ''
 
     # Load image from a given path
     def load_from_path(self, path, convert_to_png=False):
 
         debug_prefix = "[Frame.load_from_path]"
 
-        # print(debug_prefix, path)
+        tries = 0
 
         # Keep trying to read it
         while True:
+            tries += 1
+            if tries > 10:
+                print(debug_prefix, "Can't load [%s], looped [%s] times" % (path, tries))
             try:
-                # self.frame = imageio.imread(path).astype(np.uint8)
+                # Our untouched original image
                 self.original_image = Image.open(path)
-                if convert_to_png:
-                    self.original_image = self.original_image.convert("RGBA")
-                self.image = copy.deepcopy(self.original_image)
-                self.frame = np.array(self.image)
-                break
+                break # the while loop
             except Exception:
                 pass
             time.sleep(0.1)
 
+        # Warn the use rwe're not stuck anymore        
+        if tries > 10:
+            print(debug_prefix, "Could read image from path [%s]" % path)
+
+        # Sometimes we load a background in jpeg and have to process alpha composite
+        if convert_to_png:
+            self.original_image = self.original_image.convert("RGBA")
+        
+        # Copy the original image PIL's Image class and sets the frame
+        self.image = copy.deepcopy(self.original_image)
+        self.frame = np.array(self.image)
+
+        # Update width, height info
         self.height = self.image.size[0]
         self.width = self.image.size[1]
-        self.resolution = (self.width, self.height)
-        self.name = path
 
     # Wait until a file exist and then load it as a image
     def load_from_path_wait(self, path, convert_to_png=False):
@@ -93,6 +108,7 @@ class Frame():
 
         print(debug_prefix, "Saving to file: [%s]" % directory)
 
+        # JPG and PNG uses different arguments
         if '.jpg' in directory:
             jpegsave = self.image_array()
             jpegsave.save(directory, format='JPEG', subsampling=0, quality=100)
@@ -101,29 +117,38 @@ class Frame():
             save_image = self.image_array()
             save_image.save(directory, format='PNG')
     
+    # Resize this frame multiplied by a scalar
+    # @ratio: scalar to multiply
+    # @override: set the original image to the new resized one
+    # @from_current_frame: resize not the original image but the current self.frame, overrided by override
     def resize_by_ratio(self, ratio, override=False, from_current_frame=False):
 
         debug_prefix = "[Frame.resize_by_ratio]"
 
+        # Calculate the new width and height
         new_width = int(self.width * ratio)
         new_height = int(self.height * ratio)
 
         if override:
+            # Resize the original image itself and set it as the resized as well as the image var
             self.original_image = self.original_image.resize((new_width, new_height), Image.ANTIALIAS)
             self.image = self.original_image
+
+            # Standart procedure on updating this class info
             self.frame = np.array(self.image)
             self.height = self.frame.shape[0]
             self.width = self.frame.shape[1]
+
         else:
+            # Do we want self.image to be the self.image itself resized or the
             if from_current_frame:
                 toresize = self.image
             else:
                 toresize = self.original_image
+
+            # Resize and update the frame info
             self.image = toresize.resize((new_width, new_height), Image.ANTIALIAS)
             self.frame = np.array(self.image)
-
-        # print(debug_prefix, self.width, self.height, new_width, new_height, ratio)
-        # print(self.frame.shape)
 
         # Return the offset to preserve the center
         return [
@@ -131,6 +156,8 @@ class Frame():
             (self.height - new_height)/2
         ]
     
+    # Resize this frame to a fixed resolution
+    # @override: set the original image to the new resized one
     def resize_to_resolution(self, width, height, override=False):
         if override:
             self.original_image = self.original_image.resize((width, height), Image.ANTIALIAS)
@@ -142,6 +169,9 @@ class Frame():
             self.image = self.original_image.resize((width, height), Image.ANTIALIAS)
             self.frame = np.array(self.image)
     
+    # Rotate this frame by a certain angle
+    # [ WARNING ] Better only to rotate square images
+    # @override: set the original image to the new rotated
     def rotate(self, angle, override=False):
         if override:
             self.original_image = self.original_image.rotate(angle, resample=Image.BICUBIC)
@@ -151,13 +181,23 @@ class Frame():
             self.image = self.original_image.rotate(angle, resample=Image.BICUBIC)
             self.frame = np.array(self.image)
     
+    # Apply gaussian blur to the image by a certain radius
     def gaussian_blur(self, radius):
-        # print("Gaussian", radius)
         self.image = self.image.filter(ImageFilter.GaussianBlur(radius=radius))
         self.frame = np.array(self.image)
     
+    #
     # https://stackoverflow.com/questions/52702809/copy-array-into-part-of-another-array-in-numpy
-    def copy_from(self, A, B, A_start, B_start, B_end):
+    #
+    # Heavily modified, implemented out of bounds safetry (a huge headache and pain that took 5+ hours)
+    # Remember, numpy's images arrays and coordinates are the following:
+    #   - Center point (0, 0) at the top left corner
+    #     - X increases to the right
+    #     - Y increases downwards
+    #   - On reffering, Y is first then X
+    #     eg. the rectangular coordinate point (3, -4) is (4, 3) here (Y signal flipped)
+    #                                                                                       ~ Have fun
+    def copy_from(self, A, B, A_start, B_start, B_end, out_of_bounds_failsafe=True):
         """
         A_start is the index with respect to A of the upper left corner of the overlap
         B_start is the index with respect to B of the upper left corner of the overlap
@@ -166,85 +206,87 @@ class Frame():
 
         debug_prefix = "[Frame.copy_from]"
 
+        # Map the lists as numpy arrays
         A_start, B_start, B_end = map(np.asarray, [A_start, B_start, B_end])
+
+        # The shape of the copying-to coordinates
         shape = B_end - B_start
         resolution = B.shape
 
-        # print(debug_prefix, "starting from args = {%s, %s, %s, %s}" % (A_start, B_start, B_end, shape))
-        
-        # If B_start is before zero, A_start is that offset and B_start is zero
-        for i in range(2):
-            if B_start[i] < 0:
-                # print("A start changing", A_start[i], - B_start[i])
-                A_start[i] = - B_start[i]
-                # B_end[i] -= B_start[i]
-                B_start[i] = 0
-                # shape = B_end - B_start
-                # A = A[
-                #     0:shape[0],
-                #     0:shape[1]
-                # ]
+        # [ CAUTION ] If we are sure we won't be missing on arguments, this out of bounds
+        # failsafe isn't required, make sure you don't need it when not failsafing, that is
+        # you agree to only copy_from the inbounds area of this object's self.frame
+        if out_of_bounds_failsafe:
 
-        # Ignore negative shapes, error (?)
-        if any([shape[i] < 0 for i in range(2)]):
-            # print(debug_prefix, "NEGATIVE SHAPE")
-            return
-
-        # [Out of Bounds] We offsetted A_start if B_start is before zero
-        # so if the size of the shape is entirely out of bounds
-        if A_start[0] >= shape[0] or A_start[1] >= shape[1]:
-            # print("RETURN A")
-            return
-
-        # [Out of Bounds] B_start is bigger than the B's resolution on that axis
-        if B_start[0] >= resolution[0] or B_start[1] >= resolution[1]:
-            # print("RETURN B")
-            return
-
-        # # Bottom and right edge out of bounds
-
-        # End is bigger than resolution
-        # for i in range(2):
-        #     if B_end[i] >= resolution[i]:
-        #         print("A", B_end[i], resolution[i])
-        #         B_end[i] = resolution[i] - 1
-        #         print("AA", B_end[i])
-                
-        # Start + shape is bigger than resolution
-        for i in range(2):
-            if B_start[i] + shape[i] > resolution[i]:
-                # print("B", B_start[i], shape[i], resolution[i], B_end[i])
-                B_end[i] = resolution[i] - 1
-                # print("B2", B_start[i], shape[i], resolution[i], B_end[i])
-
-        # Shape is bigger than resolution, cut it
-        if any([shape[i] >= resolution[i] for i in range(2)]):
-
+            # If B_start is before zero, A_start is that offset and B_start is zero,
+            # the image is in negative coordinates for example at the top left's (-1, -1)
             for i in range(2):
-                B_end[i] = resolution[i]
+                if B_start[i] < 0:
+                    A_start[i] = - B_start[i]
+                    B_start[i] = 0
 
-            cut = [
-                min(resolution[0], B_start[0] + shape[0] + 1),
-                min(resolution[1], B_start[1] + shape[1] + 1)
-            ]
-            cut[0] += A_start[0]
-            cut[1] += A_start[1]
+            # [ NOTE ] Ignore negative shapes, error (?)
+            if any([shape[i] < 0 for i in range(2)]):
+                return
 
-            A = A[
-                0:cut[0],
-                0:cut[1]
-            ]
+            # [ Out of Bounds ] We offsetted A_start if B_start is before zero
+            # so if the size of the shape is entirely out of bounds
+            if A_start[0] >= shape[0] or A_start[1] >= shape[1]:
+                return
 
-            # print(">>>> CUT A", cut, A.shape)
-        
-        shape = B_end - B_start
-        
-        # print(debug_prefix, "Copying from, args = {%s, %s, %s, %s}" % (A_start, B_start, B_end, shape))
+            # [ Out of Bounds ] B_start is bigger than the B's resolution on that axis
+            if B_start[0] >= resolution[0] or B_start[1] >= resolution[1]:
+                return
 
+            # # Bottom and right edge out of bounds
+
+            # End is bigger than resolution TODO: does this matter?
+            # for i in range(2):
+            #     if B_end[i] >= resolution[i]:
+            #         print("A", B_end[i], resolution[i])
+            #         B_end[i] = resolution[i] - 1
+            #         print("AA", B_end[i])
+                    
+            # [ Potential error ] Start + shape is bigger than resolution
+            for i in range(2):
+                if B_start[i] + shape[i] > resolution[i]:
+                    # print("B", B_start[i], shape[i], resolution[i], B_end[i])
+                    B_end[i] = resolution[i] - 1
+                    # print("B2", B_start[i], shape[i], resolution[i], B_end[i])
+
+            # [ FIX ] Shape is bigger than resolution, cut it
+            if any([shape[i] >= resolution[i] for i in range(2)]):
+                
+                # Set the end to the resolution's end
+                for i in range(2):
+                    B_end[i] = resolution[i]
+
+                # Cut the shape to the minimum of the resolution or its starting point plus the shape
+                cut = [
+                    min(resolution[0], B_start[0] + shape[0] + 1),
+                    min(resolution[1], B_start[1] + shape[1] + 1)
+                ]
+
+                # The point we're cutting needs to be offsetted by the starting point
+                cut[0] += A_start[0]
+                cut[1] += A_start[1]
+
+                # Actually cut A, our insertion image
+                A = A[
+                    0:cut[0],
+                    0:cut[1]
+                ]
+
+            # Recalculate the shape if it has changed        
+            shape = B_end - B_start
+
+        # Catch any potential error with the transformations on the coordinates      
         try:
+            # Get a substitution tuple of the slices
             B_slices = tuple(map(slice, B_start, B_end + 1))
             A_slices = tuple(map(slice, A_start, A_start + shape + 1))
 
+            # Actually make the substitution
             B[B_slices] = A[A_slices]
 
             # print(debug_prefix, "Copied from, args = {%s, %s, %s}" % (A_start, B_start, B_end))
@@ -253,71 +295,75 @@ class Frame():
             print(debug_prefix, "Fatal error copying block: ", e)
             sys.exit(-1)
 
+    # Same as .copy_from but make an alpha composite on that point
     def overlay_transparent(self, overlay, x, y):
+        """
+        We crop this object's frame to the same size as the overlay then make an alpha composite
+        with both as PIL only makes this function with two full Image classes, this is a huge
+        optimization when looking in the context of particles, in the past we used to make a frame
+        the size of this frame's and make an alpha composite the resolution of the entire video every time
+        we wanted to process a single particle, it yielded less than 2 frames per second processing time
+        """
         
-        # print("A")
-        # print("Overlay shape", overlay.shape)
-
-        # Out of bounds
+        # [ Out of Bounds ]
         if y >= self.frame.shape[0]:
             return
         if x >= self.frame.shape[1]:
             return
 
-        # Offset the cut
+        # Offset of the cut if X or Y coordinate is negative
         x_offset = 0
         y_offset = 0
+
+        # If any are negative, set it to zero as we can't (we can but it'll get the last item)
+        # crop a list this way
         if x < 0:
             x_offset = -x
             x = 0
-            # print("X offset", x_offset)
         if y < 0:
             y_offset = -y
             y = 0
-            # print("Y offset", y_offset)
         
-        # Out of bounds, negative x bigger than shape
+        # [ Out of Bounds ] Negative x or y bigger than shape
         if x_offset >= overlay.shape[1]:
             return
         if y_offset >= overlay.shape[0]:
             return
 
+        # The cut position is this coordinate minus the negative offset plus the shape size on that coordinate
         cut_pos = [
             y + overlay.shape[0] - y_offset - 1,
             x + overlay.shape[1] - x_offset - 1
         ]
 
-        # print("CUT POS", cut_pos)
-
+        # The actual cut is made but we have to limit it to the background size if this cut pos is bigger
         cut = [
             min(cut_pos[0], self.frame.shape[0] - 1),
             min(cut_pos[1], self.frame.shape[1] - 1)
         ]
 
+        # Crop the frame
+        # NOTE: This is simply the coordinate until the cut
         crop = self.frame[
             y:cut[0],
             x:cut[1],
         ]
 
+        # Crop the overlay
+        # NOTE: This is not as simple as the frame crop as we have a offset we have to add on BOTH 
+        # cut indexes according to the actual cut and that is also subtracting the Y and X coordinates
+        # offset.
+        # Please mediate and have a good imagination on this step as it's hard to explain on words
         overlay = overlay[
             y_offset:cut[0] - y + y_offset,
             x_offset:cut[1] - x + x_offset,
         ]
         
-        # print("FRAME", self.frame)
-        # print("CROP", crop)
-
-        # print("CUT", cut)
-        # print("Y, X:", y, x)
-        # print("FRAME SHAPE", self.frame.shape)
-
-        # print("Crop shape", crop.shape)
-        # print("Overlay shape", overlay.shape)
-
+        # Calculate the alpha composite and get its array frame
         alpha_composite = Image.alpha_composite(Image.fromarray(crop), Image.fromarray(overlay))
-        # alpha_composite.save("a.png")
         alpha_composite = np.array(alpha_composite)
 
+        # Insert out alpha composite-d image to the original place on this object's frame
         self.copy_from(
             alpha_composite,
             self.frame,
@@ -326,39 +372,52 @@ class Frame():
             [y + crop.shape[0] - 1, x + crop.shape[1] - 1]
         )
 
+        # oof that was a lot
+
+    # Multiply this image's frame alpha channel by that scalar
+    # @ratio: 0 - 1 values
     def transparency(self, ratio):
+
+        # Split the original image's channels
         r, g, b, alpha = self.original_image.split()
+
+        # Multiply the alpha by that ratio
         alpha = np.array(alpha)*ratio
+
+        # Stack the arrays into a new numpy array "as image"
         image = (np.dstack((r, g, b, alpha))).astype(np.uint8)
+
+        # Load the image from the processed array and attribute the frame to it
         self.image = Image.fromarray(image)
         self.frame = image
     
+    # Apply vignetting on this image object
+    # @x: X coordinate center of the effect
+    # @y: Y coordinate center of the effect
+    # @deviation_$: sigma of the gaussian kernel
     def vignetting(self, x, y, deviation_x, deviation_y):
 
+        # Split the image into the channels
         red, green, blue, alpha = self.original_image.split()
 
+        # Get the rows and columns of the image
         img = np.array(self.original_image)
         rows, cols = img.shape[:2]
 
+        # Calculate our mask
         a = cv2.getGaussianKernel(2*cols, deviation_x)[cols - x:2 * cols - x]
         b = cv2.getGaussianKernel(2*rows, deviation_y)[rows - y:2 * rows - y]
         c = b*a.T
         d = c/c.max()
 
+        # Multiply the channels by the mask
         red = red * d
         green = green * d
         blue = blue * d
 
+        # Stack the arrays and build our new image
         image = (np.dstack((red, green, blue, alpha))).astype(np.uint8)
 
+        # Attribute this object frame and image
         self.image = Image.fromarray(image)
         self.frame = image
-
-if __name__ == "__main__":
-    f = Frame()
-    f.load_from_path("assets/tremx_assets/background.png")
-    for _ in range(100):
-        f.vignette(f.height//2, f.width//2, 300, 300)
-        print(_)
-    # f.transparency(0.95)
-    # f.save("w.png")
