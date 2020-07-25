78de59f00602340da2b461109260d65fdd63166c
Tremeschin <tremeschin@protonmail.com>
2020-07-25 18:06:15 -0400

    Explain polar coordinates, hint type

--- mmv/mmv/common/cmn_coordinates.py
+++ mmv/mmv/common/cmn_coordinates.py
@@ -19,31 +19,60 @@ this program. If not, see <http://www.gnu.org/licenses/>.
 ===============================================================================
 """
 
+from mmv.common.cmn_types import *
 import math
 
+"""
+Polar coordinates are another way of expressing points on a 2d grid.
+Instead of saying point coordinate (x, y) we say as in (r, theta) 
+whereas r is the distance from the polar ( the origin, center (0, 0) )
+and theta the angle (in radians) to what direction you distance yourself
+from the origin point.
 
-class PolarCoordinates():
-    def __init__(self):
+For example, if r=3 and theta=pi, you end up in the point (-3, 0) because
+you distanced yourself 3 units at the pi angle, 180 degrees.
+
+Because of this we can make a rectangular triangle with sides X and Y,
+hypotenuse size of r and angle theta, we can express the points as:
+
+y = r.sin(theta)
+x = r.cos(theta)
+x² + y² = r²
+
+if we divide y/x we have tan(theta) so
+
+y/x = r.sin(theta) / r.cos(theta)
+y/x = sin(theta) / cos(theta)
+y/x = tan(theta)
+
+I use this because it's easier to generate the visualization bars
+with polar coordinates as you just have to set a angle according to the
+frequency position on the ditionary and the size of the point is just the r
+which is controlled by the FFT magnitude at that point
+"""
+class PolarCoordinates:
+
+    def __init__(self) -> None:
         # Start at the point (0, 0)
         self.r = 0
         self.theta = 0
         self.round = 2
 
-    def from_r_theta(self, r, theta):
+    def from_r_theta(self, r: Number, theta: Number) -> None:
         self.r = r
         self.theta = theta
     
-    def from_rectangular(self, x, y):
+    def from_rectangular(self, x: Number, y: Number) -> None:
         self.r, self.theta = self.rectangular_to_polar(x, y)
 
-    def polar_to_rectangular(self, r, theta):
+    def polar_to_rectangular(self, r: Number, theta: Number) -> list:
         # x = r.cos(theta)
         # y = r.sin(theta)
         x = r * math.cos(theta)
         y = r * math.sin(theta)
         return [round(x, self.round), round(y, self.round)]
 
-    def rectangular_to_polar(self, x, y):
+    def rectangular_to_polar(self, x: Number, y: Number) -> list:
         # x = r.cos(theta) -> theta = cos-1 (x/r)
         # y = r.sin(theta) -> theta = sin-1 (y/r)
         # x² + y² = r² -> r = sqrt(x² + y²)
@@ -51,5 +80,5 @@ class PolarCoordinates():
         theta = math.acos(x/r)
         return [round(r, self.round), round(theta, self.round)]
     
-    def get_rectangular_coordinates(self):
+    def get_rectangular_coordinates(self) -> list:
         return self.polar_to_rectangular(self.r, self.theta)
