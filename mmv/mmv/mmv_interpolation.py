2be65ffe5e7da637909f9102d0a5246d739e6a5e
Tremeschin <tremeschin@protonmail.com>
2020-07-25 14:33:05 -0400

    Start to think about an mmv_interpolation class for simplifying interpolation methods

---
+++ mmv/mmv/mmv_interpolation.py
@@ -0,0 +1,40 @@
+"""
+===============================================================================
+
+Purpose: Interpolation simplifed for MMV
+
+===============================================================================
+
+This program is free software: you can redistribute it and/or modify it under
+the terms of the GNU General Public License as published by the Free Software
+Foundation, either version 3 of the License, or (at your option) any later
+version.
+
+This program is distributed in the hope that it will be useful, but WITHOUT
+ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+You should have received a copy of the GNU General Public License along with
+this program. If not, see <http://www.gnu.org/licenses/>.
+
+===============================================================================
+"""
+
+from mmv.common.interpolation import Interpolation
+from mmv.common.functions import Functions
+
+
+class MMVInterpolation():
+    def __init__(self, config: dict) -> None:
+        self.interpolation = Interpolation()
+        self.config = config
+
+        # Internal vars
+        self.current_value = None
+        self.target_value = None
+        self.current_step = None
+
+    def configure(self) -> None:
+        if self.config["function"] == "remaining_approach":
+            self.interpolation_function = self.interpolation.remaining_approach
+        
+    def next(self) -> None:
