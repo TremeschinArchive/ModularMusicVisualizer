f8c89fddba4bfd23f9da8a1ec40ab1677fb46ff1
Tremeschin <tremeschin@protonmail.com>
2020-12-19 23:43:29 -0400

    Add license header on experiments file, grayscale filter

--- mmv_shader/experiment.py
+++ mmv_shader/experiment.py
@@ -1,3 +1,24 @@
+"""
+===============================================================================
+
+Purpose: Developers, developers, developers
+
+===============================================================================
+
+This program is free software: you can redistribute it and/or modify it under
+the terms of the GNU General Public License as published by the Free Software
+Foundation, either version 3 of the License, or (at your option) any later
+version.
+
+This program is distributed in the hope that it will be useful, but WITHOUT
+ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+You should have received a copy of the GNU General Public License along with
+this program. If not, see <http://www.gnu.org/licenses/>.
+
+===============================================================================
+"""
+
 import mmvshader
 import os
 
@@ -10,5 +31,6 @@ mmv.mpv.input_output(
     # output_video = "out.mkv"
 )
 mmv.mpv.resolution(width = 1280, height = 720)
+mmv.mpv.add_shader(f"{mmv.DIR}/glsl/test_grayscale.glsl")
 mmv.mpv.add_shader(f"{mmv.DIR}/glsl/test_bitcrush.glsl")
 mmv.mpv.run()
